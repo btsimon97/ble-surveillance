7a6b37c1cdb146fd31512bea686ed497af3c496d
btsimon97 <14293218+btsimon97@users.noreply.github.com>
2020-09-16 14:40:04 -0400

    Improve readability of code

--- bluetooth-data.py
+++ bluetooth-data.py
@@ -5,10 +5,14 @@ kismet_api_path = "devices/views/phy-BTLE/last-time/"
 device_last_activity_timeout = 60
 
 
-api_username='kismet'
-api_password='kismet'
-device_api_args = 'json={"fields": ["btle.device","kismet.device.base.commonname","kismet.device.base.first_time","kismet.device.base.last_time","kismet.device.base.mod_time"]}'
-device_api_url = "http://" + kismet_host + "/" + kismet_api_path + "-" + str(device_last_activity_timeout) + "/devices.json"
+api_username = 'kismet'
+api_password = 'kismet'
+device_api_args = 'json={"fields": ["btle.device","kismet.device.base.commonname","kismet.device.base.first_time",' \
+                  '"kismet.device.base.last_time","kismet.device.base.mod_time"]}'
+device_api_url = "http://" + kismet_host + "/" + kismet_api_path + "-" + str(device_last_activity_timeout) \
+                 + "/devices.json"
 
-x = requests.post(device_api_url,data=device_api_args,headers = {"Content-Type": "application/x-www-form-urlencoded; charset=utf-8"},auth=(api_username,api_password))
-print(json.dumps(x.json(),indent=2))
+x = requests.post(device_api_url, data=device_api_args,
+                  headers={"Content-Type": "application/x-www-form-urlencoded; charset=utf-8"},
+                  auth=(api_username,api_password))
+print(json.dumps(x.json(), indent=2))
