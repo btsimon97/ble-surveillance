4e370f02167148870a172cd72b77dc115314fc23
juansuh <juansuhr@gmail.com>
2020-10-07 19:34:59 -0400

    added comments

--- notifications/emailer.py
+++ notifications/emailer.py
@@ -6,10 +6,15 @@ from email.mime.multipart import MIMEMultipart
 
 load_dotenv()
 
+#EMAILER CONSTANTS
 PORT = 465
 PASSWORD = os.environ.get('EMAIL_PASSWORD')
 USERNAME = os.environ.get('EMAIL_USERNAME')
 
+# MESSAGES
+#===========
+# For now these will be stored here, although they can be extracted to another file and imported
+# if this becomes too cluttered. The formate for message values is CONTENT_{TYPE}_{FORMAT}
 CONTENT_DETECTED_TEXT = """\
 Unregistered bluetooth devices have been detected!
 
@@ -31,18 +36,22 @@ CONTENT_DETECTED_HTML = """\
 </html>
 """
 
+#This function creates the message object and formats the devices onto the content
 def create_detected_message(bluetooth_devices):
     msg = MIMEMultipart("alternative")
 
     device_string_text = ""
     device_string_html = ""
 
+    #create devices string for plaintext
     for device in bluetooth_devices:
         device_string_text = device_string_text + device + "\n"
 
+    #create devices string for html
     for device in bluetooth_devices:
         device_string_html = device_string_text + "<li>" + device + "</li>"
 
+    #embed device strings into content and attach to message object, html will be tried first
     msg.attach(MIMEText(CONTENT_DETECTED_TEXT.format(text_bluetooth_elements=device_string_text), "plain"))
     msg.attach(MIMEText(CONTENT_DETECTED_HTML.format(text_bluetooth_elements=device_string_html), "html"))
 
@@ -50,6 +59,7 @@ def create_detected_message(bluetooth_devices):
 
     return msg
 
+#This function sends the final email, will be used for all types
 def send(recipient, msg, server):
     server.login(USERNAME, PASSWORD)
 
@@ -58,6 +68,7 @@ def send(recipient, msg, server):
 
     return 1
 
+#This function creates an email for warning the user about detected bluetooth devices
 def send_detected_email(recipient, bluetooth_devices):
     
     context = ssl.create_default_context()
