86a03f27d76c7986ca6857edfe16a224ed787d7b
Ben Simon <14293218+btsimon97@users.noreply.github.com>
2020-11-22 13:56:28 -0500

    Enhance the installer script
    Add apt package installation, python package installation, kismet plugin installation, etc. Essentially make the installer script do all the work needed to setup the program that used to be done manually. Also, do better detection for the user and group management stuff.

--- installer.sh
+++ installer.sh
@@ -8,34 +8,73 @@
 #Begin Script Constants Declaration
 PROG_USERNAME=btsurveillance-processing
 PROG_GROUPNAME=btsurveillance
+KISMET_APT_PKGS="kismet kismet-plugins"
+PIP_APT_PKGNAME=python3-pip
+KISMET_PIP_PKGNAME=kismetexternal
 #End Script Constants Declaration
 
+# cd to script location so file copy commands work
+cd "${0%/*}"
+
+# Check to make sure being run with root/sudo perms
 if [ `whoami` != root ]; then
-	echo This script must be run as root or with sudo for it to work.
-	echo Please re-run this script as root or with sudo
+	echo "This script must be run as root or with sudo for it to work."
+	echo "Please re-run this script as root or with sudo"
 	exit
 fi
 
-#CHeck if user already exists and abort with error if it does.
-USER_LIST="$(getent passwd | cut -d : -f 1)"
-if [[ "$USER_LIST" == *"$PROG_USERNAME"* ]]; then
-	echo "The username that this program uses already exists."
-	echo "Verify that this username isn't being used elsewhere and try again."
-	exit
+# Check if Kismet and its plugins are installed, install if not present
+dpkg -s $KISMET_APT_PKGS > /dev/null 2>&1
+if [ $? != 0 ]; then
+	echo "Installing Kismet Packages..."
+	apt install -y $KISMET_APT_PKGS
+else
+	echo "Kismet Packages already installed, continuing..."
 fi
 
-#Check if group already exists and abort with error if it does.
-GROUP_LIST="$(getent group | cut -d : -f 1)"
-if [[ "$GROUP_LIST" == *"$PROG_GROUPNAME"* ]]; then
-	echo "The group that this program uses already exists."
-	echo "Verify that this group isn't being used elsewhere and try again."
-	exit
+# Check if pip3 is installed, install if not present
+dpkg -s $PIP_APT_PKGNAME > /dev/null 2>&1
+if [ $? != 0 ]; then
+	echo "Installing PIP3..."
+	apt install -y $PIP_APT_PKGNAME
+else
+	echo "PIP3 already installed, continuing..."
 fi
 
-#Create the user and group.
-useradd btsurveillance-processing
-groupadd btsurveillance
-groupmems -g btsurveillance -a btsurveillance-processing
+# Check if kismetexternal pip package is installed, install if not present
+# If present, update to latest version.
+pip3 show kismetexternal > /dev/null 2>&1
+if [ $? != 0 ]; then
+	echo "Installing kismetexternal python package from pip..."
+	pip3 install kismetexternal
+else
+	echo "Updating installed kismetexernal python package..."
+	pip3 install kismetexternal --upgrade --upgrade-strategy=eager
+fi
+
+# Check if user already exists, create if it does not
+
+getent passwd $PROG_USERNAME > /dev/null 2>&1
+if [ $? != 0 ]; then
+	echo "Creating user $PROG_USERNAME..."
+	useradd $PROG_USERNAME
+	chsh -s /sbin/nologin $PROG_USERNAME
+fi
+
+#Check if group already exists, create if it does not
+
+getent group $PROG_GROUPNAME > /dev/null 2>&1
+if [ $? != 0 ]; then
+	echo "Creating group $PROG_GROUPNAME..."
+	groupadd $PROG_GROUPNAME
+fi
+
+#Check if user is member of group, add if not
+GROUP_MEMS="$(groupmems -g $PROG_GROUPNAME -l)"
+if [[ "$GROUP_MEMS" != *"$PROG_USERNAME"* ]]; then
+	echo "Adding $PROG_USERNAME to $PROG_GROUPNAME..."
+	groupmems -g $PROG_GROUPNAME -a $PROG_USERNAME
+fi
 
 #Deploy the tmpfiles config and reload systemd's config
 cp bt-surveillance.conf.systemd-tmpfiles /lib/tmpfiles.d/bt-surveillance.conf
@@ -52,11 +91,12 @@ systemctl daemon-reload
 #Start the Service
 systemctl start bt-surveillance.service
 
-#TODO: Add kismet detection logic and plugin install steps.
-#TODO: Add PyPi dependency installation.
+#Install the kismet_eventbus_forwarder plugin
+mkdir /lib/$(uname -m)-linux-gnu/kismet/eventbus_forwarder
+cp kismet-plugin/manifest.conf.example /lib/$(uname -m)-linux-gnu/kismet/eventbus_forwarder/manifest.conf
+cp kismet-plugin/kismet_eventbus_forwarder /usr/bin
+chmod +x /usr/bin/kismet_eventbus_forwarder
 
 #Install is done, provide post-install instructions to user and exit.
 echo "Installation Complete!"
 echo "The BT surveillance program has been installed and the service is running."
-echo "You still need to install kismet and add the plugin to receive data."
-echo "See the repo for instructions on how to do this."
