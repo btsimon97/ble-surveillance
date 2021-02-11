f925e6570c5d04b34a72b43080bc13dd666e022d
juansuh <juansuhr@gmail.com>
2021-02-11 16:36:50 -0500

    initial

--- notifications/notifications.py
+++ notifications/notifications.py
@@ -1,5 +1,9 @@
 import emailer
+import socket
 import sms
+
+HOST = '127.0.0.1'  # Localhost
+PORT = 5555        # Port to listen on
 
 def send_email(email_type, recipient, bluetooth_devices=None):
 
@@ -13,3 +17,18 @@ def send_email(email_type, recipient, bluetooth_devices=None):
 def send_sms(sms_type,recipient, bluetooth_devices = None):
     if(sms_type == "detection"):
         sms.send_detected_sms(recipient,bluetooth_devices)
+
+if __name__ == "__main__":
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print('Connected by', addr)
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                print(data.decode())
+
