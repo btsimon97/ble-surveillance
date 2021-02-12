12b3e18ebce9d27597fe0d75dd39f39e5d93f4d5
Mark Van Ree <mvanree00@gmail.com>
2021-02-11 21:26:38 -0500

    added onChange to update when current page changed

--- main.py
+++ main.py
@@ -8,18 +8,30 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.setWindowTitle("BLE Bluetooth Surveillance")
+        # on page change call onChange
+        self.ui.stackedWidget.currentChanged.connect(self.onChange)
+        # on tab button click switch to respective tab
         self.ui.btn_page_1.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.page_1))
         self.ui.btn_page_2.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.page_2))
         self.ui.btn_page_3.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.page_3))
+        # on save button click save config
         self.ui.save_1.clicked.connect(self.saveZones)
         self.ui.save_2.clicked.connect(self.saveSettings)
         self.show()
     def saveZones(self):
-        # zones config
+        # save zones config
         print("Zones saved")
     def saveSettings(self):
-        # settings config
+        # save settings config
         print("Settings saved")
+    def onChange(self,index):
+        # update the new page
+        if(index == 0):
+            print("on page 1")
+        elif(index == 1):
+            print("on page 2")
+        elif(index == 2):
+            print("on page 3")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
