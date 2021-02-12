bd44115bd030b42b6b8ca20924fabcfb9c273102
Mark Van Ree <mvanree00@gmail.com>
2021-02-11 21:10:40 -0500

    Added save buttons with function calls on click - zones, settings

--- main.py
+++ main.py
@@ -11,8 +11,15 @@ class MainWindow(QMainWindow):
         self.ui.btn_page_1.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.page_1))
         self.ui.btn_page_2.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.page_2))
         self.ui.btn_page_3.clicked.connect(lambda: self.ui.stackedWidget.setCurrentWidget(self.ui.page_3))
+        self.ui.save_1.clicked.connect(self.saveZones)
+        self.ui.save_2.clicked.connect(self.saveSettings)
         self.show()
-
+    def saveZones(self):
+        # zones config
+        print("Zones saved")
+    def saveSettings(self):
+        # settings config
+        print("Settings saved")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
