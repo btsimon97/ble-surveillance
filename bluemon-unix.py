c8f8bd344f35767a19b12a11d3253aed933eb737
btsimon97 <14293218+btsimon97@users.noreply.github.com>
2021-02-14 13:50:02 -0500

    Remove the websocket code from the UNIX socket file.
    websocket work is handled in another file so we don't need it in this one anymore.

--- bluemon-unix.py
+++ bluemon-unix.py
@@ -23,45 +23,6 @@ config = configparser.ConfigParser()
 config.read('/etc/ble-surveillance.conf')  # TODO: Implement argparse so filename is set by CLI arg instead of hardcoded
 
 
-# Identify what type of message was received (i.e. what program sent it)
-def get_message_type(message):
-    message_json = json.loads(message)
-    if ("kismet.device.base.macaddr" in message_json) or ("kismet.device.base.commonname" in message_json):
-        return "kismet"
-
-    elif ("mac" in message_json) or ("name" in message_json):
-        return "ubertooth"
-
-    else:
-        return "unknown"
-
-
-# Process Received Message
-def process_message(message):
-    message_json = json.loads(message)
-    if get_message_type(message) == "kismet":
-        device_name = message_json['kismet.device.base.commonname']
-        device_mac = message_json['kismet.device.base.macaddr']
-        detected_by_uuid = message_json['kismet.device.seenby']
-        # Determine which zone has detected the device from the configured zones
-        # Determine the notification settings for that zone
-        # If notification settings for zone demand a notification be sent:
-            # Format message into notification server JSON
-            # Connect to notification server socket
-            # Send notification message.
-
-    elif get_message_type(message) == "ubertooth":
-        print("Working...")
-        # Determine if device is known or not
-        # Grab the default Zone config settings since we don't attach a UUID to these messages
-        # If notification settings demand notification be sent:
-            # Format message into notification server JSON
-            # Connect to notification server socket
-            # Send notification message
-    else:
-        print("Received a message of unknown type, ignoring.")
-
-
 # Handle incoming connections and process received messages.
 async def handle_connection(reader, writer):
     while True:
@@ -83,67 +44,6 @@ async def unix_socket():
     async with server:
         await server.serve_forever()
 
-
-# Connect to Kismet WebSocket for device data retrieval
-async def kismet_websocket(configuration):
-    uri = ""
-    ssl_context = None # Used for TLS websocket sessions, initialized here so it can be setup later.
-
-    # message sent to kismet to get device data, along with some field simplification to reduce the data we get back.
-    # split across multiple lines for better visibility.
-    subscription_message = '{"SUBSCRIBE": "NEW_DEVICE", "fields":["kismet.device.base.seenby","kismet.device.base.macaddr","kismet.device.base.commonname","kismet.device.base.type"]}'
-
-    # build the kismet websocket URI using the configuration file data
-    if configuration['kismet'].getboolean('use_tls'):
-        uri = "wss://" + configuration['kismet']['server_name'] + ":" \
-                       + configuration['kismet']['server_port'] + "/eventbus/events.ws?" \
-                       + "user=" + configuration['kismet']['username'] \
-                       + "&password=" + configuration['kismet']['password']
-        ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-        ssl_ca_cert = pathlib.Path(__file__).with_name(configuration['kismet']['tls_cert_file'])
-        ssl_context.load_verify_locations(ssl_ca_cert)
-
-        while True:
-            try:
-                async with websockets.connect(uri, ssl=ssl_context) as websocket:
-                    print("Successfully connected to Kismet WebSocket.", flush=True)
-                    await websocket.send(subscription_message)  # send the subscribe message to kismet.
-                    # Loop forever until the connection is closed.
-                    while True:
-                        try:
-                            kismet_message = await websocket.recv()
-                            print(kismet_message, flush=True)  # replace this with a function call to the processing function.
-                        except websockets.exceptions.ConnectionClosed:
-                            print("Connection to Kismet WebSocket was closed by Kismet. Will attempt to reconnect.")
-                            break
-            except OSError:
-                print("Unable to connect to Kismet WebSocket. Maybe Kismet's not running?", flush=True)
-                print("Retrying in 1 Second...", flush=True)
-                await asyncio.sleep(1)
-
-    else:
-        uri = "ws://" + configuration['kismet']['server_name'] + ":" \
-                       + configuration['kismet']['server_port'] + "/eventbus/events.ws?" \
-                       + "user=" + configuration['kismet']['username'] \
-                       + "&password=" + configuration['kismet']['password']
-        # The actual websocket handling routine
-        while True:
-            try:
-                async with websockets.connect(uri) as websocket:
-                    print("Successfully connected to Kismet WebSocket.", flush=True)
-                    await websocket.send(subscription_message)  # send the subscribe message to kismet.
-                    # Loop forever until the connection is closed.
-                    while True:
-                        try:
-                            kismet_message = await websocket.recv()
-                            print(kismet_message)  # replace this with a function call to the processing function.
-                        except websockets.exceptions.ConnectionClosed:
-                            print("Connection to Kismet WebSocket was closed by Kismet. Will attempt to reconnect.")
-                            break
-            except OSError:
-                print("Unable to connect to Kismet WebSocket. Maybe Kismet's not running?", flush=True)
-                print("Retrying in 1 Second...", flush=True)
-                await asyncio.sleep(1)
 # End Script Functions Definition
 
 
@@ -151,14 +51,12 @@ async def kismet_websocket(configuration):
 event_loop = asyncio.get_event_loop()
 try:
     unix_socket_task = asyncio.ensure_future(unix_socket())  # Add the UNIX socket coroutine to handle incoming data
-    kismet_websocket_task = asyncio.ensure_future(kismet_websocket(config))  # Add websocket coroutine to get Kismet data
     event_loop.run_forever()
 except KeyboardInterrupt:
     pass
 finally:
     print("Shutting Down...")
     unix_socket_task.cancel()
-    kismet_websocket_task.cancel()
     event_loop.stop()
     event_loop.run_until_complete(event_loop.shutdown_asyncgens())
     event_loop.close()
