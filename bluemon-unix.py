1822c1d7c21eba26b8716ea8a2d16f6eb9f97f67
btsimon97 <14293218+btsimon97@users.noreply.github.com>
2021-03-18 23:43:17 -0400

    Port message processing code for ubertooth from middleware file.
    Port Sarah's code that handles message processing code for ubertooth messages from the old middleware to the new unix socket file.

--- bluemon-unix.py
+++ bluemon-unix.py
@@ -28,6 +28,83 @@ LISTEN_FDS = int(os.environ.get("LISTEN_FDS", 0))
 LISTEN_PID = os.environ.get("LISTEN_PID", None) or os.getpid()
 
 
+# gets device status(known or unknown) if message recieved from ubertooth
+def device_status_u(device_name, ignore_uap):
+    configParser = configparser.RawConfigParser()
+    configFilePath = r'devices.conf.example'
+    configParser.read(configFilePath)
+    device_known = False
+    device_nickname = "Unknown Device"
+    # checks all sections(devices) in configparser to see if macaddr detected is equal
+    for section in configParser.sections():
+        macaddr = configParser.get(section, 'device_macaddr')
+        if macaddr[9:] == device_name[9:]:  # last 3 parts of macaddr should always be available, check if match
+            if macaddr[6:] == device_name[6:]:  # if last 4 parts are readable and = to device, known
+                device_known = True
+                device_nickname = configParser.get(section,'device_nickname')
+            elif (macaddr[6:8] == '??') & (ignore_uap == "false"):  # if second part is ?? and we choose not to ignore uap, known
+                device_known = True
+    return device_known, device_nickname
+
+
+# determine whether the message requires notificaiton
+def message_eligibility(dev_type, configParser, zone, device_known, nickname, macaddr, ubertoothName):
+    eligibility = False
+    monitor_unknown = not device_known & (configParser.get(zone, 'alert_on_unrecognized') == 'true')
+    monitor_known = device_known & (configParser.get(zone, 'alert_on_recognized') == 'true')
+    monitor = False
+    if dev_type == 'BTLE':
+        if configParser.get(zone, 'monitor_btle_devices') == 'true':
+            monitor = True
+    else:
+        if configParser.get(zone, 'monitor_bt_devices') == 'true':
+            monitor = True
+
+    if (monitor_known | monitor_unknown) & monitor:
+        msg = nickname + " (" + ubertoothName + macaddr + ") " + " detected"
+        eligibility = True
+    else:
+        msg = "Ignoring Device event due to zone config settings."
+
+    return eligibility, msg
+
+
+# sends the appropriate message based on zone settings
+def send_message(configParser, zone, msg):
+    notification_channels = configParser.get(zone, 'notification_channels').replace(']', '').replace('[', '').replace('"', '').split(",")
+    for channel in notification_channels:
+        if channel == "email":
+            print("Sent Email, " + msg)  # placeholders  until we set up messaging service
+        elif channel == "sms":
+            print("Sent SMS,  " + msg)
+        elif channel == "signal":
+            print("Sent SMS,  " + msg)
+            # Format message into notification server JSON
+            # Connect to notification server socket
+            # Send notification message.
+
+
+# Process Received Message
+def process_message(message):
+    message_json = json.loads(message)
+    configParser = configparser.RawConfigParser()
+    configFilePath = r'zones.conf.example'
+    configParser.read(configFilePath)
+    zone = 'DEFAULT'
+    notify = False
+    dev_type = 'BT'
+    for i in range(0, len(message_json['scan_results'])):
+        device_mac = message_json['scan_results'][i]['mac']
+        try:
+            ubertooth_name =message_json['scan_results'][i]['name'] + ": "
+        except:
+            ubertooth_name=""
+        device_known, nickname = device_status_u(device_mac, configParser.get(zone, 'ignore_devices_with_unknown_uap'))
+        sendMsg, msg = message_eligibility(dev_type, configParser, zone, device_known, nickname, device_mac, ubertooth_name)
+        if(sendMsg):
+            send_message(configParser, zone, msg)
+
+
 # Handle incoming connections and process received messages.
 async def handle_connection(reader, writer):
     while True:
