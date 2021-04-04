981fd1fa7a3bc83167ddf3156f72d75374c340a4
btsimon97 <14293218+btsimon97@users.noreply.github.com>
2021-04-04 12:24:03 -0400

    Fix dictionary error in SMS notification code.
    Also update to reflect how function is called now.

--- notifications/sms.py
+++ notifications/sms.py
@@ -17,18 +17,8 @@ def send_sms_message(sms_config, recipient, msg):
     )
 
 
-# this function creates an SMS text for warning the user about detected bluetooth devices
-def send_detected_sms(sms_config, recipient, bluetooth_device):
-    msg = "We detected these unregistered bluetooth devices near your detector: "
-    for device in bluetooth_device:
-        if device == 0:
-            msg = msg + device
-        else:
-            msg = msg + ", " + device
-    return send_sms_message(recipient, msg)
-
-
 # External function called by main notification server code.
 def send_sms(sms_config, sms_type, channel_data, bluetooth_devices=None):
     if sms_type == "detection":
-        send_detected_sms(sms_config, channel_data.recipient, bluetooth_devices)
+        for recipient in channel_data['recipients']:
+            send_sms_message(sms_config, recipient, bluetooth_devices)
